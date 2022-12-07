2360711ceec88ad168d3b5eb3db141ef2d7e5134
ohah <bookyoon173@gmail.com>
2022-12-08 01:25:17 +0900

    update

--- craco.config.ts
+++ craco.config.ts
@@ -1,15 +1,3 @@
-module.exports = {
+export default {
   webpack: {},
-  jest: {
-    configure: {
-      transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest',
-      },
-      preset: 'ts-jest',
-      roots: ['<rootDir>/src'],
-      testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
-      setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-      testEnvironment: 'jsdom',
-    },
-  },
 };
---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+import { compilerOptions } from './tsconfig.json';
+
+import type { Config } from 'jest';
+
+export default async (): Promise<Config> => {
+  return {
+    extensionsToTreatAsEsm: ['.ts', '.tsx'],
+    transform: {
+      '^.+\\.(ts|tsx)$': [
+        'ts-jest',
+        {
+          supportsStaticESM: true,
+        },
+      ],
+      '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    },
+    rootDir: './',
+    preset: 'ts-jest',
+    roots: ['<rootDir>'],
+    testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    testEnvironment: 'jsdom',
+    modulePaths: [compilerOptions.baseUrl],
+    moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+  };
+};
